1126b2d4a6dd7b3213f540f78a2e552691bd2f62
luisbebop <luisbebop@gmail.com>
2015-06-19 12:59:44 -1000

    Fixed rpi2 init.d script

--- stream-server.sh
+++ stream-server.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 # /etc/init.d/stream-server.sh
 
-OUT_LOG=/dev/null
 STREAM_SERVER=/home/pi/jsmpeg/stream-server.js
 
 case "$1" in
   start)
     echo "Starting stream-server"
-    node $STREAM_SERVER abc 8082 8084 1>$OUT_LOG 2>$OUT_LOG &
-    node $STREAM_SERVER abc 8083 8085 1>$OUT_LOG 2>$OUT_LOG &
+    sudo -u pi forever start $STREAM_SERVER abc 8082 8084
+    sudo -u pi forever start $STREAM_SERVER abc 8083 8085
     ;;
   stop)
     echo "Stopping stream-server"
-    killall node
+    sudo -u pi forever stop $STREAM_SERVER abc 8082 8084
+    sudo -u pi forever stop $STREAM_SERVER abc 8083 8085
     ;;
   *)
     echo "Usage: /etc/init.d/stream-server.sh {start|stop}"
