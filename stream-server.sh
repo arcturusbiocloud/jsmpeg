e07b4f47dbba2ec3218627f36aeee7b620b14a60
luisbebop <luisbebop@gmail.com>
2015-06-30 19:51:30 -1000

    Disabled camera auto focus.

--- stream-server.sh
+++ stream-server.sh
@@ -4,6 +4,8 @@
 case "$1" in
   start)
     echo "Starting stream-server"
+    su pi -c 'v4l2-ctl -d /dev/video1 -c focus_auto=0'
+    su pi -c 'v4l2-ctl -d /dev/video2 -c focus_auto=0'
     su pi -c 'node /home/pi/jsmpeg/stream-server.js abc 8082 8084 < /dev/null &'
     su pi -c 'node /home/pi/jsmpeg/stream-server.js abc 8083 8085 < /dev/null &'
     ;;
